b77b39f6e495ec60cea466b96a2663308c7451a7
Rashad Philizaire <rashadphilizaire@gmail.com>
2023-10-21 18:20:56 -0500

    added basic pop modal

--- doc-expert/src/content.tsx
+++ doc-expert/src/content.tsx
@@ -1,10 +1,23 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut
+} from "@/components/ui/command"
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
 
 import { CountButton } from "~features/count-button"
 
+import "~style.css"
+
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.plasmo.com/*"]
+  matches: ["https://docs.convex.dev/*", "https://news.ycombinator.com/*"]
 }
 
 export const getStyle = () => {
@@ -15,9 +28,23 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
-    </div>
+    <CommandDialog open={true}>
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>Calendar</CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>Profile</CommandItem>
+          <CommandItem>Billing</CommandItem>
+          <CommandItem>Settings</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
   )
 }
 
